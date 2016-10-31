4578ba64d261e5f6c68c463f19dc8e16cdd50479
tydkdev <tydkdev90@gmail.com>
2016-10-31 07:35:23 -0400

    Choices working as form

--- client/dev/colors/controllers/choices.js
+++ client/dev/colors/controllers/choices.js
@@ -5,6 +5,13 @@
     .controller('ChoicesController', function ($scope, ChoicesService) {
       $scope.choices;
 
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.list.push(this.text);
+          $scope.text = '';
+        }
+      };
+
       ChoicesService.getChoices()
         .then(function (choices) {
           $scope.choices = choices;
